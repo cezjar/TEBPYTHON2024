b0837f737274b6b5009a8a27e2ffe74192cdcdb6
cezjar <Cezary.Jaremek@gmail.com>
2024-03-24 13:45:07 +0100

    Dodano rozwiązanie laboratoriów z modułu 2 z kursu netacad

--- Zajęcia 1.py
+++ Zajęcia 1.py
@@ -19,3 +19,96 @@ print(round(feet, 2), 'stóp')
 print(round(miles, 2), 'mil')
 
 #Należy wykonać wszystkie laboratoria z modułu 2 z kursu python na netacad.
+
+#LAB1
+print('Wlazl kotek na plotek')
+print('Cezary Jaremek')
+
+#LAB2
+
+print("Kurs","Programowania","w",sep = "***", end = "...")
+print("Pythonie")
+
+#LAB3
+print("    *    " * 2 + "\n" +
+      "   * *   " * 2 + "\n" +
+      "  *   *  " * 2 + "\n" +
+      " *     * " * 2 + "\n" +
+      "***   ***" * 2 + "\n" +
+      "  *   *  " * 2 + "\n" +
+      "  *   *  " * 2 + "\n" +
+      "  *****  " * 2)
+
+#LAB4
+print('"Ucze się" \n ""jezyka"" \n """Python"""')
+
+#LAB5
+pomaranczowy_krol = 3
+agnieszka = 5
+adam = 6
+print(pomaranczowy_krol, agnieszka, adam, sep = ',')
+
+pomaranczowy_razem = pomaranczowy_krol + agnieszka + adam
+print(pomaranczowy_razem)
+print(f"Całkowita liczba pomarańczy {pomaranczowy_razem}")
+
+#LAB6
+kilometry = 12.25
+mile = 7.38
+
+mile_na_kilometry =  mile * 1.61
+kilometry_na_mile =  kilometry / 1.61
+
+print(mile, "mi to", round(mile_na_kilometry, 2), "km")
+print(kilometry, "km to", round(kilometry_na_mile, 2), "mi")
+
+
+#LAB7
+x =  1
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x -1
+print("y =", y)
+
+#LAB8
+#Ten program oblicza liczbę sekund w danej liczbie godzin.
+godziny = 3 #liczba godzin
+sekundy_w_godzinie = 3600 #liczba sekund w 1 godzinie
+
+print("Godzin: ", godziny) # wyświetla ilość godzin
+print("Sekund w godzinach: ", godziny * sekundy_w_godzinie)  # wyswietla ilość sekund w zadanej liczbie godzin
+print("Do widzenia")
+# to jest koniec programu, który oblicza liczbę sekund w 3 godzinach
+
+#LAB9
+
+a = 2.1
+b = 3.1
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print("\nI to by było na tyle!")
+
+#LAB10
+
+x = float(input("Wprowadź wartość dla x: "))
+
+y = (1/(x+(1/(x+(1/(x+(1/x)))))))
+
+print("y =", y)
+
+
+#LAB11
+h = int(input("Czas startu (godziny): "))
+m = int(input("Czas startu (minuty): "))
+d = int(input("Czas trwania wydarzenia (minuty): "))
+
+minuta_zakonczenia = (m + d) % 60
+ilosc_godzin_do_dodania = (m + d) // 60
+
+godzina_zakonczenia = (h + ilosc_godzin_do_dodania) % 24
+
+
+print(godzina_zakonczenia, minuta_zakonczenia, sep=':')
